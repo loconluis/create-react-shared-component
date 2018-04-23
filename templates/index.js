41e7302f9e7e094e159248c904bc4397adaffa8a
Luis Locon <luis.locon@gmail.com>
2018-04-23 09:48:33 -0600

    Adding a npmIgnore template and license.

--- templates/index.js
+++ templates/index.js
@@ -2,5 +2,6 @@ const appTemplate = require('./App')
 const gitIgnoreTemplate = require('./gitIgnore')
 const webpackTemplate = require('./webpackConfig')
 const esLintTemplate = require('./esLint')
+const npmIgnoreTemplate = require('./npmIgnore')
 
-module.exports = { appTemplate, gitIgnoreTemplate, webpackTemplate, esLintTemplate }
+module.exports = { appTemplate, gitIgnoreTemplate, webpackTemplate, esLintTemplate, npmIgnoreTemplate }
